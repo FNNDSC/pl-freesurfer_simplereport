d07be9abe04bcd18a0a168887b8ffc63048babb2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2022-06-30 18:03:16 -0400

    Move init to own module, improve logic

--- freesurfer_simplereport.py
+++ freesurfer_simplereport.py
@@ -14,11 +14,10 @@ __version__ = __pkg.version
 import os
 from importlib.resources import files
 import sys
-import nibabel as nib
-import numpy as np
-import collections
-import re
-import pandas as pd
+# import nibabel as nib
+# import numpy as np
+#import collections
+# import pandas as pd
 from yattag import Doc
 import pdfkit
 from fpdf import FPDF
@@ -28,8 +27,10 @@ from    pfmisc._colors          import Colors
 
 import  pudb
 
-import  lut
-from    lut                     import lookuptable
+from    init                    import start
+from    mgz                     import fmgzio
+from    report                  import template
+from    filter                  import convert
 
 DISPLAY_TITLE = r"""
        _         __                                __              _                 _                                _
@@ -42,10 +43,6 @@ DISPLAY_TITLE = r"""
 |_|                                                     |______|              |_|                   |_|
 """
 
-
-PFMlogger       = None
-LOG             = None
-LUT             = None
 
 parser = ArgumentParser(description='A ChRIS DS plugin that generates a report table (in various formats) off a FreeSurfer annotation/segmentation volume',
                         formatter_class=ArgumentDefaultsHelpFormatter)
@@ -101,64 +98,7 @@ parser.add_argument('-V', '--version', action='version',
                     version=f'%(prog)s {__version__}')
 
 
-def init(options: Namespace, inputdir: Path, outputdir: Path) -> dict:
-    """Perform some initializations, most notably verify/check on the
-    FreeSurfer lookup table file.
-
-    Args:
-        options (Namespace): CLI namespace
-        inputdir (Path): the plugin inputdir
-        outputdir (Path): the plugin outputdir
-
-    Returns:
-        dictionary: initialization status and lookup table file location.
-    """
-
-    global PFMlogger, LOG, LUT
-
-    d_ret               = {
-        'isOK':             True,
-        'lookupTableFile':  None
-    }
-    path_lutfile        : Path = None
-    PFMlogger           = pfmisc.debug(
-                                            verbosity   = int(options.verbosity),
-                                            within      = 'main',
-                                            syslog      = True
-                                        )
-    LOG                 = PFMlogger.qprint
-    LOG("initializing...")
-    for k,v in options.__dict__.items():
-         LOG("%25s:  [%s]" % (k, v))
-    LOG("")
-    pudb.set_trace()
-    if len(options.internalLUTpath):
-        inputdir    = Path(options.internalLUTpath)
-    if inputdir.is_dir():
-        LOG("lookup table inputdir  = %s" % str(inputdir))
-        LOG("checking on lookup table file... ")
-        try:
-            lutmapper = PathMapper.file_mapper(inputdir, outputdir, glob = options.lut)
-        except Exception as e:
-            LOG(f"{e}", comms = "error")
-            d_ret['isOK'] = False
-        if not lutmapper.is_empty():
-            for path_lutfile, path_outlut in lutmapper: pass
-        else:
-            LOG(f"I could not find {options.lut} in the input space", comms = 'error')
-            d_ret['isOK'] = False
-    else:
-        LOG(f"Lookup table parent directory {inputdir} not found!", comms = 'error')
-        d_ret['isOK'] = False
-    if path_lutfile:
-        LOG(path_lutfile)
-        d_ret['lookupTableFile']    = path_lutfile
-        LUT = lookuptable.lookuptable(lut = path_lutfile, log = LOG)
-    else:
-        LOG(f"No lookup table file found!", comms = 'error')
-    return d_ret
-
-def report_generate(options: Namespace, inputfile: Path, outputfile: Path):
+def segmentation_process(options: Namespace, inputfile: Path, outputfile: Path):
     """Main entry point for generating a report in various formats based
     off a FreeSurfer annotation file.
 
@@ -167,28 +107,33 @@ def report_generate(options: Namespace, inputfile: Path, outputfile: Path):
         inputfile (Path): The input file to process
         outputfile (Path): The corresponding outputfile path
     """
-    LOG(f"Generating a report off {inputfile}")
+
+    start.LOG(f"Processing {inputfile}...")
+    pudb.set_trace()
+    annot           = fmgzio.mgz(inputfile, log = start.LOG)
+    pudb.set_trace()
+    masterreport    = template.template(mgz = annot, lut = start.LUT, log = start.LOG)
 
 
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
-    parser=parser,
-    title='FreeSurfer Simple Reporting',
-    category='',                 # ref. https://chrisstore.co/plugins
-    min_memory_limit='100Mi',    # supported units: Mi, Gi
-    min_cpu_limit='1000m',       # millicores, e.g. "1000m" = 1 CPU core
-    min_gpu_limit=0              # set min_gpu_limit=1 to enable GPU
+    parser              = parser,
+    title               = 'FreeSurfer Simple Reporting',
+    category            = '',                   # ref. https://chrisstore.co/plugins
+    min_memory_limit    = '100Mi',              # supported units: Mi, Gi
+    min_cpu_limit       = '1000m',              # millicores, e.g. "1000m" = 1 CPU core
+    min_gpu_limit       = 0                     # set min_gpu_limit=1 to enable GPU
 )
 def main(options: Namespace, inputdir: Path, outputdir: Path):
     print(DISPLAY_TITLE, file=sys.stderr)
     print(f'Options: {options}', file=sys.stderr)
 
-    if init(options, inputdir, outputdir)['isOK']:
+    if start.init(options, inputdir, outputdir)['isOK']:
         mapper = PathMapper.file_mapper(inputdir, outputdir,
                             glob    = options.annotation
         )
         for input, output in mapper:
-            report_generate(options, input, output)
+            segmentation_process(options, input, output)
 
 
 if __name__ == '__main__':
