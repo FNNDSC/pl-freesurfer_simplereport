63df39b3a48ecb899786500a9c0ab3ae3f385840
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-07-01 13:36:03 -0400

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='freesurfer_simplereport',
-    version='1.0.2',
+    version='1.1.0',
     description='A ChRIS DS plugin that generates a report table (in various formats) off a FreeSurfer annotation/segmentation volume',
     author='FNNDSC',
     author_email='rudolph.pienaar@childrens.harvard.edu',
