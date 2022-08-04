7c24889012c6e64ea2ac6497feff59674df5c7d3
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-08-04 17:17:06 -0400

    Add mostly null-ish testing

--- tests/test_example.py
+++ tests/test_example.py
@@ -13,44 +13,7 @@ str_notes = '''
 
 '''
 
-
-#options         = parser.parse_args(['--inputFilter', '*dcm'])
-
-
-#def test_inputDir2File_do_skips_dirs_without_hits(tmp_path: Path):
-    #inputdir    = tmp_path / 'inputdir'
-    #outputdir   = tmp_path / 'outputdir'
-    #inputdir.mkdir()
-    #outputdir.mkdir()
-
-    #file        = inputdir / 'file.txt'
-    #file.write_text('I am not a DICOM file.')
-
-    #assert inputDir2File_do(options, (inputdir, outputdir)) is None
-
-
-#def test_inputDir2File_do_returns_a_dicom(tmp_path: Path):
-    #inputdir    = tmp_path / 'inputdir'
-    #outputdir   = tmp_path / 'outputdir'
-    #inputdir.mkdir()
-    #outputdir.mkdir()
-    ## create example data
-    #dicoms = {
-        #inputdir / 'file1.dcm',
-        #inputdir / 'file2.dcm',
-        #inputdir / 'file3.dcm'
-    #}
-
-    #for dicom in dicoms:
-        #dicom.touch()
-
-    #actual = inputDir2File_do(options, (inputdir, outputdir))
-    #assert actual is not None
-    #actual_dicom, actual_outputdir = actual
-    #assert actual_dicom in dicoms
-    #assert actual_outputdir == outputdir
-
-def test_main(mocker, tmp_path: Path):
+def test_main(tmp_path: Path):
     """
     Simulated test run of the app.
     """
@@ -61,10 +24,6 @@ def test_main(mocker, tmp_path: Path):
 
     options     = parser.parse_args(['--verbosity', '1'])
 
-    #mock_print  = mocker.patch('builtins.print')
+    # This doesn't really test much of anything.
     main(options, inputdir, outputdir)
-    # mock_print.assert_has_calls([call(DISPLAY_TITLE), call("Option ")])
 
-    expected_output_file = outputdir / 'success.txt'
-    # assert expected_output_file.exists()
-    # assert expected_output_file.read_text() == 'did nothing successfully!'
